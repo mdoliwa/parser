b7aecb01432c6272cf06c10c262caf8c8a95c87a
Marcin Doliwa <mdoliwa@gmail.com>
2020-11-12 22:39:58 +0100

    Add #page_views and #unique_page_views methods

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,2 +1,35 @@
 class LogParser
+  attr_reader :path, :data
+
+  def initialize(path)
+    @path = path
+    @data = {}
+  end
+
+  def prepare_data
+    File.readlines(path).each do |line|
+      page, ip = line.split(/\s+/)
+
+      data[page] = data[page] ? data[page] << ip : [ip]
+    end
+  end
+
+  def page_views
+    prepare_data if data.empty?
+
+    data.
+      sort_by{|page, ips| -ips.length}.
+      map{|page, ips| "#{page} #{ips.length}"}.
+      join("\n")
+  end
+
+  def unique_page_views
+    prepare_data if data.empty?
+
+    data.
+      sort_by{|page, ips| -ips.uniq.length}.
+      map{|page, ips| "#{page} #{ips.uniq.length}"}.
+      join("\n")
+  end
 end
+
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,5 +1,24 @@
 require 'log_parser'
 
 describe LogParser do
+  describe '#page_views' do
+    it "shows page views" do
+      path = "spec/files/webserver.log"
+
+      expected_output = ["/about/1 4", "/home 3", "/contact 2"].join("\n")
+
+      expect(LogParser.new(path).page_views).to eq expected_output
+    end
+  end
+
+  describe '#unique_page_views' do
+    it "shows unique page views" do
+      path = "spec/files/webserver.log"
+
+      expected_output = ["/home 3", "/contact 2", "/about/1 1"].join("\n")
+
+      expect(LogParser.new(path).unique_page_views).to eq expected_output
+    end
+  end
 end
 
